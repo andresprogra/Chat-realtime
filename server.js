35273d61b8c8e0531ee2228b2a5c717e5f57c275
Andrés Dominguez <andres_gomez@live.com.mx>
2016-03-22 21:29:42 -0700

    Utilizar path y fs asíncrono

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 //JavaScript estricto
 'use strict'
 //Variables constantes, porqué los módulos que requerimos no queremos que cambien en algún momento.
-const  http = require('http')
+const http = require('http')
 const fs = require('fs')
 const path = require('path')
 //Accedemos a las variables de entorno para ver en que puerto podemos escuchar
@@ -10,7 +10,7 @@ const path = require('path')
 const port = process.env.PORT || 8080
 //Permitirá hacer un servidor Web con io.js
 //Se crea un servidor con iojs
-const server = http.createServer(onRequest)
+const server = http.createServer()
 //Nuestro servidor escuchará en un puerto, en el puerto 8080
 
 //Se inician los eventos
@@ -21,12 +21,15 @@ server.listen(port)
 //Res = response, respuestas
 function onRequest(req, res){
 	let fileName = path.join(__dirname,'public','index.html')
-	fs.readFile(fileName, function (err, file){
-		if (err){
-			return res.end(err.message)
-		}
-		res.end(file)
+	
+	res.setHeader('Content-Type', 'text/html')
+	let rs = fs.createReadStream(fileName)
+	rs.pipe(res)
+
+	rs.on('error', function(err){
+		res.end(err.message)
 	})
+
 }
 function onListening(){
 	console.log("Escuchando en el puerto " + port)
