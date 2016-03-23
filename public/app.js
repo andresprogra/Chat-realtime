157cfad162cbf7bfdfcaf69c5c35b86fdf1f87f3
Andrés Dominguez <andres_gomez@live.com.mx>
2016-03-22 21:49:15 -0700

    Servir rutas: index y app.js

---
+++ public/app.js
@@ -0,0 +1,5 @@
+const record = document.querySelector('#record')
+record.addEventListener('click', function(e){
+	e.preventDefault()
+	console.log("Boton activado")
+})
--- server.js
+++ server.js
@@ -20,18 +20,41 @@ server.listen(port)
 //Req = request, peticiones. 
 //Res = response, respuestas
 function onRequest(req, res){
+	let uri = req.url
+	if (uri.startsWith('/index') || uri === '/index.html' || uri === '/')
+	{
+		return serveIndex(res)
+	}
+	if (uri === '/app.js'){
+		return serverApp(res)
+	}
+	res.statusCode = 404
+	res.end(`404 Not found ${uri}`)
+
+
+}
+function serveIndex(res){
 	let fileName = path.join(__dirname,'public','index.html')
-	
 	res.setHeader('Content-Type', 'text/html')
 	let rs = fs.createReadStream(fileName)
 	rs.pipe(res)
-
 	rs.on('error', function(err){
 		res.end(err.message)
 	})
+}
+function serverApp (res){
+	let app = path.join(__dirname,'public','app.js')
+	
+	res.setHeader('Content-Type', 'text/JavaScript')
+	let rs = fs.createReadStream(app)
+	rs.pipe(res)
 
+	rs.on('error', function(err){
+		res.setHeader('Content-Type', 'text/plain')
+		res.end(err.message)
+	})
 }
 function onListening(){
-	console.log("Escuchando en el puerto " + port)
+	console.log(`Escuchando en el puerto ${port}`)
 }
 /*Fin del servidor básico */
