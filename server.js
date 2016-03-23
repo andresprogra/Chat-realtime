6fd74dc8d4df2c2aac5efa2af8b78fc340af6630
Andrés Dominguez <andres_gomez@live.com.mx>
2016-03-22 18:09:55 -0700

    Crear el servidor web

---
+++ server.js
@@ -0,0 +1,32 @@
+/* Inicio del servidor básico */
+
+//JavaScript estricto
+'use strict'
+//Variables constantes, porqué los módulos que requerimos no queremos que cambien en algún momento.
+const  http = require('http')
+//Accedemos a las variables de entorno para ver en que puerto podemos escuchar
+//Si el primer valor es falso se asignará el 8080
+const port = process.env.PORT || 8080
+//Permitirá hacer un servidor Web con io.js
+//Se crea un servidor con iojs
+const server = http.createServer(onRequest)
+//Nuestro servidor escuchará en un puerto, en el puerto 8080
+
+//Se inician los eventos
+server.on('request', onRequest)
+server.on('listening', onListening)
+server.listen(port)	
+//Req = request, peticiones. 
+//Res = response, respuestas
+function onRequest(req, res){
+	res.end("Hola io.js")
+}
+function onListening(){
+	console.log("Escuchando en el puerto " + port)
+}
+
+
+
+
+
+/*Fin del servidor básico */
