ecab2c1d9ecde05da7a30b0883b6943bfd7fe1a0
Andrés Dominguez <andres_gomez@live.com.mx>
2016-03-23 17:34:33 -0700

    Decodificar imágenes y guardarlas en el disco

--- helper/video.js
+++ helper/video.js
@@ -1,8 +1,16 @@
 'use strict'
+const os = require('os')
+const fs = require('fs')
+const path = require('path')
 const async = require('async')
+const uuid = require('uuid')
+const dataURIBuffer = require('data-uri-to-buffer')
 const EventEmitter = require('events').EventEmitter
 module.exports = function (images){
 	let events = new EventEmitter()
+	let count = 0
+	let baseName = uuid.v4()
+	let tmpDir = os.tmpdir()
 	async.series([
 		decodeImages,
 		createVideo,
@@ -11,7 +19,17 @@ module.exports = function (images){
 
 	], converFinished)
 	function decodeImages(done){
-		done()
+		async.eachSeries(images, decodeImage, done)
+
+	}
+	function decodeImage(image,done){
+		//La imagen la vamos a convertir a buffer
+		let fileName = `${baseName}-${count++}.jpg`
+		let buffer = dataURIBuffer(image)
+		let ws = fs.createWriteStream(path.join(tmpDir, fileName))
+		ws.on('error',done)
+			.end(buffer,done)
+		events.emit('log', `Converting ${fileName}`)
 
 	}
 	function createVideo(done){
